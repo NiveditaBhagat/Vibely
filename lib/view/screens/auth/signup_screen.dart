cb2994ddbda117c55f2e23f577f3b03c53caa11b
Nivedita Bhagat <navyabhaga01@gmail.com>
2023-04-03 23:39:46 +0530

    third commit

--- lib/view/screens/auth/signup_screen.dart
+++ lib/view/screens/auth/signup_screen.dart
@@ -15,67 +15,74 @@ TextEditingController _confirmpasswordController = new TextEditingController();
       body: Container(
         color: backgroundColor,
         alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-           Stack(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+             
+            Text('Vibely', 
+            style: TextStyle(
+              fontFamily: 'Billabong', 
+              fontSize: 65,
+            color: Colors.white),),
+            Stack(
+              children: [
+                CircleAvatar(
+                 backgroundImage: AssetImage('assets/default.jpg'),
+                  radius: 65,
+                ),
+                Positioned(bottom: 0,right: 0, child: Icon(Icons.photo_camera, color: Colors.white,size: 30,))
+              ],
+             ),
+            SizedBox(height: 39,),
               
-            ],
-           ),
-          Text('Vibely', 
-          style: TextStyle(
-            fontFamily: 'Billabong', 
-            fontSize: 70,
-          color: Colors.white),),
-          SizedBox(height: 39,),
-      
-       
-            Container(
-              margin: EdgeInsetsDirectional.symmetric(horizontal: 20),
-              child: TextInputField(
-                controller: _emailController,
-                myLabelText: "Email",
-                myIcon: Icons.email,
+               
+              Container(
+                margin: EdgeInsetsDirectional.symmetric(horizontal: 20),
+                child: TextInputField(
+                  controller: _emailController,
+                  myLabelText: "Email",
+                  myIcon: Icons.email,
+                ),
               ),
-            ),
-            SizedBox(height: 20,),
-            Container(
-              margin: EdgeInsetsDirectional.symmetric(horizontal: 20),
-              child: TextInputField(
-                controller: _setpasswordController,
-                myLabelText: "Set Password",
-                myIcon: Icons.lock,
-                toHide: true,
-              ),
-              
-            ),
-            SizedBox(height: 20,),
-               Container(
-              margin: EdgeInsetsDirectional.symmetric(horizontal: 20),
-              child: TextInputField(
-                controller: _confirmpasswordController,
-                myLabelText: "Confirm Password",
-                myIcon: Icons.lock,
-                toHide: true,
-              ),
-              
-            ),
               SizedBox(height: 20,),
               Container(
-              margin: EdgeInsetsDirectional.symmetric(horizontal: 20),
-              child: TextInputField(
-                controller: _emailController,
-                myLabelText: "Username",
-                myIcon: Icons.person,
+                margin: EdgeInsetsDirectional.symmetric(horizontal: 20),
+                child: TextInputField(
+                  controller: _setpasswordController,
+                  myLabelText: "Set Password",
+                  myIcon: Icons.lock,
+                  toHide: true,
+                ),
+                
               ),
-            ),
-             SizedBox(height: 40,),
-            Button(
-              text: 'SIGN UP',
-              onTap: (){},
-            )
-          ],
+              SizedBox(height: 20,),
+                 Container(
+                margin: EdgeInsetsDirectional.symmetric(horizontal: 20),
+                child: TextInputField(
+                  controller: _confirmpasswordController,
+                  myLabelText: "Confirm Password",
+                  myIcon: Icons.lock,
+                  toHide: true,
+                ),
+                
+              ),
+                SizedBox(height: 20,),
+                Container(
+                margin: EdgeInsetsDirectional.symmetric(horizontal: 20),
+                child: TextInputField(
+                  controller: _emailController,
+                  myLabelText: "Username",
+                  myIcon: Icons.person,
+                ),
+              ),
+               SizedBox(height: 40,),
+              Button(
+                text: 'SIGN UP',
+                onTap: (){},
+              )
+            ],
+          ),
         ),
       ),
     );
