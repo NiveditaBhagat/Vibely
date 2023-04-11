d3d03767879aca6c7c2cf7d62354338a874a908c
Nivedita Bhagat <navyabhaga01@gmail.com>
2023-04-11 15:31:21 +0530

    commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -15,3 +15,11 @@ getRandomColor() => [
 const backgroundColor = Colors.black;
 var buttonColor = Colors.red[400];
 const borderColor = Colors.grey;
+
+const pageindex=[
+  'Home',
+  'Search',
+  'Upload Vedio',
+  'Messages',
+  'Profile',
+];
--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:vibely/constants.dart';
+import 'package:vibely/view/widgets/custom_add_icon.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+   HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+ int pageIdx=0;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+  
+      backgroundColor: Colors.black,
+      bottomNavigationBar: BottomNavigationBar(
+     type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.grey[900],
+         unselectedItemColor: Colors.white,
+            
+            selectedItemColor: Color(0xFFE07699),
+        onTap: (value) {
+          setState(() {
+            pageIdx=value;
+          });
+        },
+        currentIndex: pageIdx,
+        items: [
+          BottomNavigationBarItem(
+            icon:Icon(Icons.home, size: 25,color: Colors.white,),
+            label: 'Home', 
+            ),
+           
+            BottomNavigationBarItem(
+            icon:Icon(Icons.search, size: 25,color: Colors.white,),
+            label: 'Search', 
+            ),
+             BottomNavigationBarItem(
+            icon:CustomAddIcon(),
+            label: '', 
+            ),
+             BottomNavigationBarItem(
+            icon:Icon(Icons.message, size: 25,color: Colors.white,),
+            label: 'Message', 
+            ),
+             BottomNavigationBarItem(
+            icon:Icon(Icons.person, size: 25,color: Colors.white,),
+            label: 'Profile', 
+            ),
+            
+            
+
+
+        ],
+        ),
+        body: Center(
+          child: Text(pageindex[pageIdx], style: TextStyle(color: Colors.white),) ,
+        ),
+    );
   }
 }
