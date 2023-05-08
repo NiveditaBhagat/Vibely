2cf909eef820df7d1fc9a82eda4a96d369c621b2
Nivedita Bhagat <navyabhaga01@gmail.com>
2023-05-08 23:49:33 +0530

    sign up

--- lib/view/screens/auth/login_screen.dart
+++ lib/view/screens/auth/login_screen.dart
@@ -1,60 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:vibely/constants.dart';
 import 'package:vibely/controller/auth_controller.dart';
+import 'package:vibely/view/screens/auth/signup_screen.dart';
 import 'package:vibely/view/widgets/button.dart';
 import 'package:vibely/view/widgets/text_input.dart';
+
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
-TextEditingController _emailController = new TextEditingController();
-TextEditingController _passwordController = new TextEditingController();
+
+  TextEditingController _emailController = new TextEditingController();
+  TextEditingController _passwordController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      
-      body: Container(
-        color: backgroundColor,
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-          Text('Vibely', 
-          style: TextStyle(
-            fontFamily: 'Billabong', 
-            fontSize: 70,
-          color: Colors.white),),
-          SizedBox(height: 10,),
-      
-            SizedBox(height: 29,),
-            Container(
-              margin: EdgeInsetsDirectional.symmetric(horizontal: 20),
-              child: TextInputField(
-                controller: _emailController,
-                myLabelText: "Email",
-                myIcon: Icons.email,
+      body: SingleChildScrollView(
+        child: Container(
+          height: screenHeight,
+          color: backgroundColor,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Vibely',
+                style: TextStyle(
+                  fontFamily: 'Billabong',
+                  fontSize: screenHeight * 0.1,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            SizedBox(height: 20,),
-            Container(
-              margin: EdgeInsetsDirectional.symmetric(horizontal: 20),
-              child: TextInputField(
-                controller: _passwordController,
-                myLabelText: "Password",
-                myIcon: Icons.lock,
-                toHide: true,
+              SizedBox(height: screenHeight * 0.03),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: screenHeight * 0.03),
+                child: TextInputField(
+                  controller: _emailController,
+                  myLabelText: "Email",
+                  myIcon: Icons.email,
+                ),
               ),
-              
-            ),
-            SizedBox(height: 40,),
-            Button(
-              text: 'LOGIN',
-              onTap: (){
-                AuthController.instance.login(_emailController.text, _passwordController.text);
-              },
-              height: 55,
-              width: 300,
-            )
-          ],
+              SizedBox(height: screenHeight * 0.02),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: screenHeight * 0.03),
+                child: TextInputField(
+                  controller: _passwordController,
+                  myLabelText: "Password",
+                  myIcon: Icons.lock,
+                  toHide: true,
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.04),
+              Container(
+                child: Button(
+                  text: 'LOGIN',
+                  onTap: () {
+                    AuthController.instance.login(
+                        _emailController.text, _passwordController.text);
+                  },
+                  height: screenHeight * 0.08,
+                  width: screenHeight * 0.5,
+                ),
+                height: 50,
+              ),
+               SizedBox(height: 20),
+                        Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an account? ',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=> SignUpScreen()));
+                      },
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/view/screens/auth/signup_screen.dart
+++ lib/view/screens/auth/signup_screen.dart
@@ -3,6 +3,7 @@ import 'package:vibely/constants.dart';
 import 'package:vibely/controller/auth_controller.dart';
 import 'package:vibely/view/widgets/button.dart';
 import 'package:vibely/view/widgets/text_input.dart';
+
 class SignUpScreen extends StatefulWidget {
   SignUpScreen({super.key});
 
@@ -11,18 +12,16 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
-TextEditingController _emailController = new TextEditingController();
-
-TextEditingController _setpasswordController = new TextEditingController();
-
-TextEditingController _confirmpasswordController = new TextEditingController();
-
-TextEditingController _usernameController = new TextEditingController();
+  TextEditingController _emailController = new TextEditingController();
+  TextEditingController _setpasswordController = new TextEditingController();
+  TextEditingController _confirmpasswordController = new TextEditingController();
+  TextEditingController _usernameController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      
       body: Container(
         color: backgroundColor,
         alignment: Alignment.center,
@@ -30,76 +29,84 @@ TextEditingController _usernameController = new TextEditingController();
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-             
-            Text('Vibely', 
-            style: TextStyle(
-              fontFamily: 'Billabong', 
-              fontSize: 65,
-            color: Colors.white),),
-            InkWell(
-              onTap: (){
-                AuthController.instance.pickImage();
-              },
-              child: Stack(
-                children: [
-                  CircleAvatar(
-                   backgroundImage: AssetImage('assets/default.jpg'),
-                    radius: 65,
-                  ),
-                  Positioned(bottom: 0,right: 0, child: Icon(Icons.photo_camera, color: Colors.white,size: 30,))
-                ],
-               ),
-            ),
-            SizedBox(height: 39,),
-              
-               
+              Text(
+                'Vibely',
+                style: TextStyle(
+                  fontFamily: 'Billabong',
+                  fontSize: screenHeight * 0.1,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.03),
+              InkWell(
+                onTap: () {
+                  AuthController.instance.pickImage();
+                },
+                child: Stack(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/default.jpg'),
+                      radius: screenHeight * 0.12,
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Icon(
+                        Icons.photo_camera,
+                        color: Colors.white,
+                        size: screenHeight * 0.03,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.039),
               Container(
-                margin: EdgeInsetsDirectional.symmetric(horizontal: 20),
+                margin: EdgeInsets.symmetric(horizontal: screenHeight * 0.03),
                 child: TextInputField(
                   controller: _emailController,
                   myLabelText: "Email",
                   myIcon: Icons.email,
                 ),
               ),
-              SizedBox(height: 20,),
+              SizedBox(height: screenHeight * 0.02),
               Container(
-                margin: EdgeInsetsDirectional.symmetric(horizontal: 20),
+                margin: EdgeInsets.symmetric(horizontal: screenHeight * 0.03),
                 child: TextInputField(
                   controller: _setpasswordController,
                   myLabelText: "Set Password",
                   myIcon: Icons.lock,
                   toHide: true,
                 ),
-                
               ),
-              SizedBox(height: 20,),
-                 Container(
-                margin: EdgeInsetsDirectional.symmetric(horizontal: 20),
+              SizedBox(height: screenHeight * 0.02),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: screenHeight * 0.03),
                 child: TextInputField(
                   controller: _confirmpasswordController,
                   myLabelText: "Confirm Password",
                   myIcon: Icons.lock,
                   toHide: true,
                 ),
-                
               ),
-                SizedBox(height: 20,),
-                Container(
-                margin: EdgeInsetsDirectional.symmetric(horizontal: 20),
+              SizedBox(height: screenHeight * 0.02),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: screenHeight * 0.03),
                 child: TextInputField(
                   controller: _usernameController,
                   myLabelText: "Username",
                   myIcon: Icons.person,
                 ),
               ),
-               SizedBox(height: 40,),
+              SizedBox(height: screenHeight * 0.04),
               Button(
                 text: 'SIGN UP',
-                onTap: (){
-                  AuthController.instance.SignUp(_usernameController.text, _emailController.text, _setpasswordController.text, AuthController.instance.proimg);
+                onTap: () {
+                  AuthController.instance.SignUp(_usernameController.text,
+                      _emailController.text, _setpasswordController.text, AuthController.instance.proimg);
                 },
-              height: 55,
-              width: 300,
+                height: screenHeight * 0.08,
+                width: screenHeight * 0.5,
               )
             ],
           ),
